83ec60e58e7d2146d0b6cbfbcc4ecef968220e6f
jiang shu yi <3a932083@gm.student.ncut.edu.tw>
2022-11-02 10:42:53 +0800

    設定各網頁路徑連結

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,6 +45,9 @@
                     </svg>
                 </div>
 
+                <a href="/about">about</a>
+                <a href="/news">news</a>
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
@@ -57,6 +60,7 @@
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
                                 </div>
+
                             </div>
                         </div>
 
